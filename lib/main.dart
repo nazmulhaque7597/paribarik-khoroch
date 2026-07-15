393908a76be7b85422c6018646b91e5b35f4186c
nazmulhaque7597 <nazmulhaque7597@gmail.com>
2026-07-15 09:19:58 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'add_expense.dart';
 
 void main() {
   runApp(const ParibarikKhoroch());
@@ -21,52 +22,102 @@ class ParibarikKhoroch extends StatelessWidget {
   }
 }
 
+
 class HomeScreen extends StatelessWidget {
+
   const HomeScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       appBar: AppBar(
-        title: const Text('💰 পারিবারিক খরচ'),
+        title: const Text("💰 পারিবারিক খরচ"),
         centerTitle: true,
+        backgroundColor: Colors.green,
       ),
 
+
       body: Column(
+
         children: [
-          const SizedBox(height: 30),
+
+          const SizedBox(height:20),
+
 
           Card(
+
             margin: const EdgeInsets.all(15),
+
             color: Colors.green.shade100,
+
             child: const ListTile(
+
               title: Text(
-                'আজকের মোট খরচ',
-                style: TextStyle(fontSize: 20),
+                "আজকের মোট খরচ",
+                style: TextStyle(fontSize:20),
               ),
+
               trailing: Text(
-                '৳ 0',
+                "৳ 0",
                 style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold),
+                  fontSize:22,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+
+            ),
+
+          ),
+
+
+          const SizedBox(height:30),
+
+
+          const Text(
+            "প্রতিদিনের খরচ এখানে রাখুন",
+            style: TextStyle(
+              fontSize:20,
             ),
           ),
 
-          const SizedBox(height: 20),
-
-          const Text(
-            'নতুন খরচ যোগ করতে নিচের + চাপুন',
-            style: TextStyle(fontSize: 18),
-          ),
         ],
+
       ),
+
 
       floatingActionButton: FloatingActionButton(
+
         backgroundColor: Colors.green,
-        onPressed: () {},
+
         child: const Icon(Icons.add),
+
+
+        onPressed: (){
+
+
+          Navigator.push(
+
+            context,
+
+            MaterialPageRoute(
+
+              builder: (context)=> const AddExpense(),
+
+            ),
+
+          );
+
+
+        },
+
       ),
+
+
     );
+
   }
+
 }
